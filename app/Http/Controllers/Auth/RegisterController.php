8ba0ac2e8f3b8cff8fbee5003d97bdc73677d984
Kevin Winarko <saberkevin27@gmail.com>
2018-12-19 17:00:01 +0700

    fix error migration in app service provider to support emojis, migration typo column, drop if exists wrong table name

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,70 +2,35 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Rules\EndsWith;
 use App\User;
 use App\Http\Controllers\Controller;
+use http\Env\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
-    use RegistersUsers;
-
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    protected function createUser(Request $data)
     {
-        $this->middleware('guest');
-    }
+        $validator = Validator::make($data->all,
+            [
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|unique:users',
+                'password' => 'required|string|min:confirmed',
+                'password_confirmation' => 'required|string',
+                'phone' => 'required|numeric',
+                'gender' => 'required|in:male,female',
+                'address' => ['required|string', new EndsWith('Street')],
+                'profile_pict' => 'required|mimes:jpeg,png,jpg',
+                'birthday' => 'required|date',
+                'agree' => 'accepted'
+            ]);
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator(array $data)
-    {
-        return Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-        ]);
-    }
+        if($validator->fails())
+        {
+            return redirect()->back()->withErrors($validator);
+        }
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\User
-     */
-    protected function create(array $data)
-    {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => bcrypt($data['password']),
-        ]);
     }
 }
